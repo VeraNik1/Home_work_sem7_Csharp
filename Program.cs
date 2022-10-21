8034f41a0bffae4ca9bbdddf3b166ef03c7eb5e1
Vera <solnce288888@gmail.com>
2022-10-21 21:56:02 +0300

    num 47+

--- Program.cs
+++ Program.cs
@@ -5,15 +5,16 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
 Console.WriteLine("Введите количество строк: ");
 int rows = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите количество столбцов: ");
 int columns = int.Parse(Console.ReadLine()!);
-double[,] array = GetArray(rows, columns, -10, 10);
+double[,] array = GetArrayDouble(rows, columns, -10, 10);
 PrintArray(array);
 
 
-double[,] GetArray(int n, int m, double MinValue, double MaxValue)
+double[,] GetArrayDouble(int n, int m, double MinValue, double MaxValue)
 {
     double[,] result = new double[n, m];
     for (int i = 0; i < n; i++)
