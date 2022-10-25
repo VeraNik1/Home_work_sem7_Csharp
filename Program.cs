9316271d10b10d7e3b6ec0db3ff27d51dbcb37cb
Vera <solnce288888@gmail.com>
2022-10-25 22:56:53 +0300

    finish

--- Program.cs
+++ Program.cs
@@ -5,41 +5,40 @@
 // // 0,5 7 -2 -0,2
 // // 1 -3,3 8 -9,9
 // // 8 7,8 -7,1 9
-// Console.WriteLine("Задача 47");
-// Console.WriteLine();
-// Console.WriteLine("Введите количество строк: ");
-// int rows = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Введите количество столбцов: ");
-// int columns = int.Parse(Console.ReadLine()!);
-// double[,] array = GetArrayDouble(rows, columns, -10, 10);
-// PrintArrayDouble(array);
-// Console.WriteLine();
+
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+double[,] array = GetArrayDouble(rows, columns, -10, 10);
+PrintArrayDouble(array);
+Console.WriteLine();
 
 
-// double[,] GetArrayDouble(int n, int m, double MinValue, double MaxValue)
-// {
-//     double[,] result = new double[n, m];
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < m; j++)
-//         {
-//             result[i, j] = new Random().NextDouble() * (MaxValue - MinValue) + MinValue;
-//         }
+double[,] GetArrayDouble(int m, int n, double MinValue, double MaxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * (MaxValue - MinValue) + MinValue;
+        }
 
-//     }
-//     return result;
-// }
-// void PrintArrayDouble(double[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($"{array[i, j]:f1} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+    }
+    return result;
+}
+void PrintArrayDouble(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:f1} ");
+        }
+        Console.WriteLine();
+    }
+}
 // // Задача 50. Напишите программу, которая на вход 
 // // принимает позиции элемента в двумерном массиве, и 
 // // возвращает значение этого элемента или же указание, 
@@ -53,59 +52,59 @@
 
 // // i = 4, j = 2 -> такого числа в массиве нет
 // // i = 1, j = 2 -> 2
-// Console.WriteLine("Задача 50");
-// Console.WriteLine();
-// int row = new Random().Next(2, 15);
-// int column = new Random().Next(1, 15);
-// Console.WriteLine("Сгенерированный массив: ");
-// int[,] array50 = GetArrayInt(row, column, 1, 50);
-// PrintArrayInt(array50);
-// Console.WriteLine();
-// Console.WriteLine("Введите номер строки i: ");
-// int Row = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Введите номер столбца j: ");
-// int Column = int.Parse(Console.ReadLine()!);
-// Console.WriteLine(GetItemFromArray(array50, Row, Column));
-// Console.WriteLine();
+
+
+int row = new Random().Next(2, 15);
+int column = new Random().Next(1, 15);
+Console.WriteLine("Сгенерированный массив: ");
+int[,] array50 = GetArrayInt(row, column, 1, 50);
+PrintArrayInt(array50);
+Console.WriteLine();
+Console.WriteLine("Введите номер строки i: ");
+int Row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите номер столбца j: ");
+int Column = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetItemFromArray(array50, Row, Column));
+Console.WriteLine();
 
 
 
-// String GetItemFromArray(int[,] array, int I, int J)
-// {
-//     if ((I >= array.GetLength(0) || I < 0) || (J >= array.GetLength(1) || J < 0))
-//     {
-//         return $"i = {I}, j = {J} -> эемента с указанными индексами нет в сгенерированном массиве";
-//     }
-//     else
-//     {
-//         return $"i = {I}, j = {J} -> {array[I, J]}";
-//     }
-// }
+String GetItemFromArray(int[,] array, int I, int J)
+{
+    if ((I >= array.GetLength(0) || I < 0) || (J >= array.GetLength(1) || J < 0))
+    {
+        return $"i = {I}, j = {J} -> эемента с указанными индексами нет в сгенерированном массиве";
+    }
+    else
+    {
+        return $"i = {I}, j = {J} -> {array[I, J]}";
+    }
+}
 
-// int[,] GetArrayInt(int n, int m, int minValue, int maxValue)
-// {
-//     int[,] result = new int[n, m];
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < m; j++)
-//         {
-//             result[i, j] = new Random().Next(minValue, maxValue + 1);
-//         }
+int[,] GetArrayInt(int n, int m, int minValue, int maxValue)
+{
+    int[,] result = new int[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
 
-//     }
-//     return result;
-// }
-// void PrintArrayInt(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($"{array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+    }
+    return result;
+}
+void PrintArrayInt(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
 
 // // Задача 52. Задайте двумерный массив из целых чисел. 
@@ -116,39 +115,38 @@
 // // 8 4 2 4
 // // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-// Console.WriteLine("Задача 52");
-// Console.WriteLine();
-// int rowAr = new Random().Next(2, 10);
-// int columnAr = new Random().Next(1, 10);
-// Console.WriteLine("Сгенерированный массив: ");
-// int[,] array52 = GetArrayInt(rowAr, columnAr, 1, 10);
-// PrintArrayInt(array52);
-// Console.WriteLine();
-// GetMeanOfColumns(array52);
-// Console.WriteLine();
 
-// void GetMeanOfColumns(int[,] array)
-// {
-//     Console.Write("Среднее арифметическое каждого столбца: ");
-//     for (int j = 0; j < array.GetLength(1); j++)
-//     {
-//         double total = 0;
-//         for (int i = 0; i < array.GetLength(0); i++)
-//         {
-//             total += array[i, j];
-//         }
-//         Console.Write($"{(double)total / array.GetLength(0):f1}");
-//         if (j == array.GetLength(1) - 1)
-//         {
-//             Console.Write(".");
-//         }
-//         else
-//         {
-//             Console.Write("; ");
-//         }
-//     }
+int rowAr = new Random().Next(2, 10);
+int columnAr = new Random().Next(2, 10);
+Console.WriteLine("Сгенерированный массив: ");
+int[,] array52 = GetArrayInt(rowAr, columnAr, 1, 10);
+PrintArrayInt(array52);
+Console.WriteLine();
+GetMeanOfColumns(array52);
+Console.WriteLine();
 
-// }
+void GetMeanOfColumns(int[,] array)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double total = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            total += array[i, j];
+        }
+        Console.Write($"{(double)total / array.GetLength(0):f1}");
+        if (j == array.GetLength(1) - 1)
+        {
+            Console.Write(".");
+        }
+        else
+        {
+            Console.Write("; ");
+        }
+    }
+
+}
 
 // Задача 60. ...Сформируйте трёхмерный массив из 
 // неповторяющихся двузначных чисел. Напишите программу,
@@ -160,68 +158,67 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-// Console.WriteLine("Задача 60");
-// Console.WriteLine();
-// Console.WriteLine("Введите размеры трехмерного массива:");
-// Console.WriteLine("первый: ");
-// int x = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("второй: ");
-// int y = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("третий: ");
-// int z = int.Parse(Console.ReadLine()!);
-// int[,,] array60 = GetArrayThreeD(x, y, z);
-// PrintArrayInt3D(array60);
+
+Console.WriteLine("Введите размеры трехмерного массива:");
+Console.WriteLine("первый: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("второй: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine("третий: ");
+int z = int.Parse(Console.ReadLine()!);
+int[,,] array60 = GetArrayThreeD(x, y, z);
+PrintArrayInt3D(array60);
 
 
 //получение 3-х мерного массива из двухзначных уникальных чисел
-// int[,,] GetArrayThreeD(int n, int m, int p){
-//     int[,,] result = new int[n, m, p];
-//     int[] Temparray = GetUniqeNum(n, m, p);
-//     if(Temparray[0] < 0){
-//         Console.WriteLine("Количество элементов массива превышает количество уникальных двухзначных чисел. Поробуйте уменьшить размеры массива");
-//         return new int[,,]{};}
-//     else{
-//     int index = 0;
-//     while(index < n*m*p){
-//         for (int i = 0; i < n; i++)
-//             {for (int j = 0; j < m; j++)
-//                 {for (int k = 0; k < p; k++){
-//                     result[i, j, k] = Temparray[index];
-//                     index++;
-//             }
-//         }}}
-//         return result;}
-//         }
+int[,,] GetArrayThreeD(int n, int m, int p){
+    int[,,] result = new int[n, m, p];
+    int[] Temparray = GetUniqeNum(n, m, p);
+    if(Temparray[0] < 0){
+        Console.WriteLine("Количество элементов массива превышает количество уникальных двухзначных чисел. Поробуйте уменьшить размеры массива");
+        return new int[,,]{};}
+    else{
+    int index = 0;
+    while(index < n*m*p){
+        for (int i = 0; i < n; i++)
+            {for (int j = 0; j < m; j++)
+                {for (int k = 0; k < p; k++){
+                    result[i, j, k] = Temparray[index];
+                    index++;
+            }
+        }}}
+        return result;}
+        }
  // метод для получения уникальных двухзначных чисел    
-//  int[] GetUniqeNum(int n, int m, int p)
-//  {  int index = 0;
-//     int[] result = new int[n*m*p];
-//     if(m*n*p > 90){
-//         return new int[]{-1};
-//     }
-//     else{
-//     while(index < n*m*p){
-//     {int temp = new Random().Next(10, 100);
-//         if(Array.IndexOf(result, temp) == -1){
-//             result[index] = temp;
-//             index++;
-//         }
-//  }}}
-//  return result;}
+ int[] GetUniqeNum(int n, int m, int p)
+ {  int index = 0;
+    int[] result = new int[n*m*p];
+    if(m*n*p > 90){
+        return new int[]{-1};
+    }
+    else{
+    while(index < n*m*p){
+    {int temp = new Random().Next(10, 100);
+        if(Array.IndexOf(result, temp) == -1){
+            result[index] = temp;
+            index++;
+        }
+ }}}
+ return result;}
 
 //метод печати 3-х мерного массива
-// void PrintArrayInt3D(int[,,] array)
-// {
-//         for (int k = 0; k < array.GetLength(2); k++)
-//             {for (int i = 0; i < array.GetLength(0); i++)
-//                 {for (int j = 0; j < array.GetLength(1); j++){
+void PrintArrayInt3D(int[,,] array)
+{
+        for (int k = 0; k < array.GetLength(2); k++)
+            {for (int i = 0; i < array.GetLength(0); i++)
+                {for (int j = 0; j < array.GetLength(1); j++){
         
-//             Console.Write($"{array[i, j, k]}({i}, {j}, {k}) ");
-//             }
-//         Console.WriteLine();
-//     }
-// }
-// }
+            Console.Write($"{array[i, j, k]}({i}, {j}, {k}) ");
+            }
+        Console.WriteLine();
+    }
+}
+}
     
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
